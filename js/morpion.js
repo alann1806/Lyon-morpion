27d1812be79590ea1b397788ec917ffd829479f0
alann1806 <allubet@edenschool.com>
2019-10-04 15:36:40 +0200

    dsl

--- js/morpion.js
+++ js/morpion.js
@@ -1,4 +1,12 @@
 let aQuiLeTour= 'X';
+
+let joueur1 = 0;
+let joueur2 = 0;
+let joueur1Element = document.getElementById('joueur1');
+let joueur2Element = document.getElementById('joueur2');
+
+
+
 const cases = document.querySelectorAll('.case');
 
 
@@ -29,8 +37,24 @@ cases.forEach(function (element) {
         || ((cases[6].innerHTML == "X") && (cases[7].innerHTML == "X") && (cases[8].innerHTML == "X"))
         || ((cases[1].innerHTML == "X") && (cases[4].innerHTML == "X") && (cases[7].innerHTML == "X"))
         || ((cases[2].innerHTML == "X") && (cases[5].innerHTML == "X") && (cases[8].innerHTML == "X"))) {
-            alert("joueur X à gagné");
-            window.location.reload();
+            joueur1++;
+            joueur1Element.innerHTML = joueur1;
+            cases.forEach(function () {
+                cases[0].innerHTML = "";
+                cases[1].innerHTML = "";
+                cases[2].innerHTML = "";
+                cases[3].innerHTML = "";
+                cases[4].innerHTML = "";
+                cases[5].innerHTML = "";
+                cases[6].innerHTML = "";
+                cases[7].innerHTML = "";
+                cases[8].innerHTML = "";
+            });
+            alert("+ 1 point au joueur X");
+            // vider les cases
+            
+
+
         }//pour joueur O
          else if (((cases[0].innerHTML == "O") && (cases[1].innerHTML == "O") && (cases[2].innerHTML == "O")) //--
         || ((cases[3].innerHTML == "O") && (cases[4].innerHTML == "O") && (cases[5].innerHTML == "O")) //__
@@ -40,13 +64,24 @@ cases.forEach(function (element) {
         || ((cases[6].innerHTML == "O") && (cases[7].innerHTML == "O") && (cases[8].innerHTML == "O"))
         || ((cases[1].innerHTML == "O") && (cases[4].innerHTML == "O") && (cases[7].innerHTML == "O"))
         || ((cases[2].innerHTML == "O") && (cases[5].innerHTML == "O") && (cases[8].innerHTML == "O"))) {
-            alert("joueur O à gagné");
-            window.location.reload();
+            joueur2++;
+            joueur2Element.innerHTML = joueur2;
+            cases.forEach(function () {
+                cases[0].innerHTML = "";
+                cases[1].innerHTML = "";
+                cases[2].innerHTML = "";
+                cases[3].innerHTML = "";
+                cases[4].innerHTML = "";
+                cases[5].innerHTML = "";
+                cases[6].innerHTML = "";
+                cases[7].innerHTML = "";
+                cases[8].innerHTML = "";
+            });
+            alert("+ 1 point au joueur O");
+            // vider les cases
         }
-        
     })
 });
-
 
 
 
